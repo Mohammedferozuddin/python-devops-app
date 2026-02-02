40692212476a017292928c0e03b2ccd096d79892
mohammedferozuddin <feroz.md10010@gmail.com>
2026-02-02 16:17:06 +0530

    add working flask app

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Project Deployed on AWS 🚀"
+    return """
+    <h1>🚀 DevOps App is Running!</h1>
+    <p>Deployed using Azure DevOps Pipeline + Docker + AWS EC2</p>
+    """
+
+@app.route("/health")
+def health():
+    return {"status": "OK"}
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
