1bdaf5e289b5b2aa06bada90c18d38d7f65207a1
mohammedferozuddin <feroz.md10010@gmail.com>
2026-02-04 14:54:33 +0530

    Initial project upload

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>🚀 DevOps App is Running!</h1>
-    <p>Deployed using Azure DevOps Pipeline + Docker + AWS EC2</p>
-    """
+    return render_template("home.html")
 
-@app.route("/health")
-def health():
-    return {"status": "OK"}
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
